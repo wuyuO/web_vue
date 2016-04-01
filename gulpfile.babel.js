9939876221ce23c78640afb27734ca392fcd5626
wuyuO <huahua_lanh@163.com>
2016-04-01 12:26:10 +0800

    add watch babel js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,22 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import watch from 'gulp-watch'
 
-gulp.task('default',() => {
+gulp.task('transform',() => {
   return gulp.src('src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch',() => {
+  return gulp.src('src/**/*.js')
+    .pipe(watch('src/**/*.js',{
+        verbose:true
+      }))
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
+})
+
+gulp.task('default',() => {
+  gulp.start('transform');
 });
--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
 router.get('',(req,res,next) => {
-  res.end('Hello World!!!');
+  res.end('Hello World!');
 });
 
 app.use('',router);
